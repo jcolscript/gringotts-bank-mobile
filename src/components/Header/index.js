25ed01fd952e4870758a9fd46ddd70e2d19b835d
José Carlos <jcolscript@gmail.com>
2020-06-20 05:47:57 -0300

    estilizando o header da aplicacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container} from './styles';
+import {Container, Top, Logo, Title} from './styles';
+
+import logo from '~/assets/logo.png';
 
 export default function Header() {
-  return <Container />;
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Harry Potter</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#fff" />
+    </Container>
+  );
 }
