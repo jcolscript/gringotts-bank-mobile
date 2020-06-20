4ccf54ec4717515379b4970edaf315f6f529d3f3
José Carlos <jcolscript@gmail.com>
2020-06-20 06:28:59 -0300

    criando e estilizando as tabs de acao

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container} from './styles';
+import {Container, TabsContaier, TabItem, TabText} from './styles';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContaier>
+        <TabItem>
+          <Icon name="phone-iphone" size={24} color="#f9d423" />
+          <TabText>Recarga de celular</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#f9d423" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#f9d423" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#f9d423" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="local-atm" size={24} color="#f9d423" />
+          <TabText>Pagar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#f9d423" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContaier>
+    </Container>
+  );
 }
