03f77e0bc13bd1ccaa41ee7283e79c14ae3f5a81
José Carlos <jcolscript@gmail.com>
2020-06-21 23:22:55 -0300

    criando componente central do app

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  height: 100%;
+  height: 100px;
   margin-top: 20px;
 `;
 
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Trasferencia de R$ 20,00 recebida de Hermione hoje as 19:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
